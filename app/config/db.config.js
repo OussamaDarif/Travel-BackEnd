182916d22c33a43f1fb1925b397385fddf02a49f
OussamaDarif <odarif48@gmail.com>
2024-01-17 15:42:26 +0100

    database conf

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: "mongodb://localhost:27017/travelbyrec_db"
+  // url: "mongodb+srv://invyscode_achraf:MLJAchraf9898_@travelbyrec-db.l6djzle.mongodb.net/travelbyrec_db"
+};
